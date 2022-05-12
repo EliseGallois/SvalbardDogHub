a477ccc2daf7484f8bbec23d00f3930241369f37
EliseGallois <elise.gallois94@gmail.com>
2022-05-12 15:11:01 +0200

    Elise updated phenology plots

--- scripts/04_Phenology.R
+++ scripts/04_Phenology.R
@@ -22,21 +22,26 @@ as_tibble(head(pheno))
 pheno$site <- recode(pheno$site, PYR_pig = 'HH_pyrpig')
 pheno$site <- recode(pheno$site, REF_tem = 'BC_tem')
 
+# remove sites not suitable for pheno analysis (no greening curves)
+pheno <- pheno %>% 
+  filter(!(site %in% c("REF_fest", "REF_lein", "REF_odin", "REF_rein")))
+
 
 #### 50% ####
 splines50 <- pheno %>% 
   mutate(type = substr(site, start = 1, stop = 2)) %>%    # get land use type classification 
   mutate(Year = lubridate::ymd(year, truncated = 2L)) %>%  # set year to date type
   mutate(unique_id  = paste(sample.id,year)) %>% 
-  dplyr::group_by(Year, sample.id) %>% 
-  mutate(ratio = round(spl.fit, digits = 2)) %>% 
-  filter(ratio == 0.38)
+  dplyr::group_by(unique_id) %>% 
+  dplyr::mutate(ratio = max(ndvi)*0.5) %>% 
+  mutate(ratio = round(ratio, digits = 1)) %>% 
+  mutate(ndvi = round(ndvi, digits = 1)) %>%
+  filter(ndvi == ratio) 
 
 
 splines50a <- splines50 %>% 
   mutate(site_doy_id  = paste(unique_id,doy)) %>% 
   distinct(site_doy_id, .keep_all= TRUE) %>% # remove duplicate rows
-  subset(ave(unique_id, unique_id, FUN = length) > 1) %>% 
   dplyr::group_by(unique_id) %>% 
   arrange(doy) %>% 
   dplyr::mutate(
@@ -47,10 +52,10 @@ splines50a <- splines50 %>%
 splines50a$range<-(splines50a$senescence-splines50a$greenup)
 
 # remove any range <5
-splines50a <- splines50a[which(splines50a$range >= 5),]
+splines50a <- splines50a[which(splines50a$range >= 6),]
 
   
-# wide data  
+# long data  
 thresh50 <-  pivot_longer(splines50a,cols = c("greenup", "senescence"),
                names_to = "phase",
                values_to = "phase_doy")  
@@ -61,20 +66,20 @@ thresh50$site_doy_id <- paste(thresh50$site_doy_id,thresh50$phase)
 # remove duplicate rows
 thresh50 = thresh50[!duplicated(thresh50$site_doy_id),]
    
-#### 25% ####
-splines25 <- pheno %>% 
+#### 30% ####
+splines30 <- pheno %>% 
   mutate(type = substr(site, start = 1, stop = 2)) %>%    # get land use type classification 
   mutate(Year = lubridate::ymd(year, truncated = 2L)) %>%  # set year to date type
   mutate(unique_id  = paste(sample.id,year)) %>% 
-  dplyr::group_by(Year, sample.id) %>% 
-  mutate(ratio = round(spl.fit, digits = 2)) %>% 
-  filter(ratio == 0.19)
+  dplyr::group_by(unique_id) %>% 
+  dplyr::mutate(ratio = max(ndvi)*0.3) %>% 
+  mutate(ratio = round(ratio, digits = 1)) %>% 
+  mutate(ndvi = round(ndvi, digits = 1)) %>%
+  filter(ndvi == ratio) 
 
-
-splines25a <- splines25 %>% 
+splines30a <- splines30 %>% 
   mutate(site_doy_id  = paste(unique_id,doy)) %>% 
   distinct(site_doy_id, .keep_all= TRUE) %>% # remove duplicate rows
-  subset(ave(unique_id, unique_id, FUN = length) > 1) %>% 
   dplyr::group_by(unique_id) %>% 
   arrange(doy) %>% 
   dplyr::mutate(
@@ -82,37 +87,41 @@ splines25a <- splines25 %>%
     senescence = dplyr::last(doy)) 
 
 # range between greenup and senescence
-splines25a$range<-(splines25a$senescence-splines25a$greenup)
+splines30a$range<-(splines30a$senescence-splines30a$greenup)
 
 # remove any range <5
-splines25a <- splines25a[which(splines25a$range >= 5),]
+splines30a <- splines30a[which(splines30a$range >= 6),]
+
 
 # long data  
-thresh25 <-  pivot_longer(splines25a,cols = c("greenup", "senescence"),
+thresh30 <-  pivot_longer(splines30a,cols = c("greenup", "senescence"),
                           names_to = "phase",
                           values_to = "phase_doy")  
 
 # identify duplicate rows
-thresh25$site_doy_id <- paste(thresh25$site_doy_id,thresh25$phase)
+thresh30$site_doy_id <- paste(thresh30$site_doy_id,thresh30$phase)
 
 # remove duplicate rows
-thresh25 = thresh25[!duplicated(thresh25$site_doy_id),]
+thresh30 = thresh30[!duplicated(thresh30$site_doy_id),]
+
+
+##### 5 - PLOT TRENDS BY TYPE ####
 
 # yearly trends per site 
-
 (greenup50 <- ggplot(thresh50) +
     geom_point(aes(x = year, y = phase_doy, colour = phase), alpha = 0.5, size = 2) +
-    geom_smooth(method=lm, aes(x = year,  y = phase_doy, colour = phase))) + 
+    geom_smooth(method=lm, aes(x = year,  y = phase_doy, fill = phase, colour = phase))) + 
     ylab("Phenophase DOY (50% of Maximum Curviture) \n") +
     xlab("Year") +
-    scale_color_viridis_d(option = "viridis") +
-    scale_fill_viridis_d(option = "viridis") +
-    theme_classic() +
+    scale_color_manual(values = c("#09E88F", "#E0C707")) +
+   scale_fill_manual(values = c("#09E88F", "#E0C707")) +
+  theme_classic() +
   facet_wrap(vars(type))
 ggsave('figures/50_sen_ts.jpg', width = 9, height = 5, units = 'in', dpi = 400)
 
 
-(greenup25 <- ggplot(thresh25) +
+
+(greenup30 <- ggplot(thresh30) +
     geom_point(aes(x = year, y = phase_doy, colour = phase), alpha = 0.5, size = 2) +
     geom_smooth(method=lm, aes(x = year,  y = phase_doy, colour = phase))) + 
   ylab("Phenophase DOY (25% of Maximum Curviture) \n") +
@@ -123,6 +132,6 @@ ggsave('figures/50_sen_ts.jpg', width = 9, height = 5, units = 'in', dpi = 400)
   facet_wrap(vars(type))
 ggsave('figures/25_sen_ts.jpg', width = 9, height = 5, units = 'in', dpi = 400)
 
-
+(phen <- grid.arrange(greenup25, greenup50, ncol = 1))
 
 
