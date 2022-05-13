3830c6183ea9b8ed1312d740f64cc5ef19d80250
EliseGallois <elise.gallois94@gmail.com>
2022-05-13 09:30:30 +0200

    Elise adds 25% greenup and senesence

--- scripts/04_Phenology.R
+++ scripts/04_Phenology.R
@@ -23,8 +23,8 @@ pheno$site <- recode(pheno$site, PYR_pig = 'HH_pyrpig')
 pheno$site <- recode(pheno$site, REF_tem = 'BC_tem')
 
 # remove sites not suitable for pheno analysis (no greening curves)
-pheno <- pheno %>% 
-  filter(!(site %in% c("REF_fest", "REF_lein", "REF_odin", "REF_rein")))
+#pheno <- pheno %>% 
+ # filter(!(site %in% c("REF_fest", "REF_lein", "REF_odin", "REF_rein")))
 
 
 #### 50% ####
@@ -66,21 +66,59 @@ thresh50$site_doy_id <- paste(thresh50$site_doy_id,thresh50$phase)
 # remove duplicate rows
 thresh50 = thresh50[!duplicated(thresh50$site_doy_id),]
    
+#### 25% ####
+splines25 <- pheno %>% 
+  mutate(type = substr(site, start = 1, stop = 2)) %>%    # get land use type classification 
+  mutate(Year = lubridate::ymd(year, truncated = 2L)) %>%  # set year to date type
+  mutate(unique_id  = paste(sample.id,year)) %>% 
+  dplyr::group_by(unique_id) %>% 
+  dplyr::mutate(ratio = max(ndvi)*0.25) %>% 
+  mutate(ratio = round(ratio, digits = 2)) %>% 
+  mutate(ndvi = round(ndvi, digits = 2)) %>%
+  filter(ndvi == ratio) 
+
+
+splines25a <- splines25 %>% 
+  mutate(site_doy_id  = paste(unique_id,doy)) %>% 
+  distinct(site_doy_id, .keep_all= TRUE) %>% # remove duplicate rows
+  dplyr::group_by(unique_id) %>% 
+  arrange(doy) %>% 
+  dplyr::mutate(
+    greenup = dplyr::first(doy),
+    senescence = dplyr::last(doy)) 
+
+# range between greenup and senescence
+splines25a$range<-(splines25a$senescence-splines25a$greenup)
+
+# remove any range <5
+splines25a <- splines25a[which(splines25a$range >= 6),]
+
+
+# long data  
+thresh25 <-  pivot_longer(splines25a,cols = c("greenup", "senescence"),
+                          names_to = "phase",
+                          values_to = "phase_doy")  
+
+# identify duplicate rows
+thresh25$site_doy_id <- paste(thresh25$site_doy_id,thresh25$phase)
+
+# remove duplicate rows
+thresh25 = thresh25[!duplicated(thresh25$site_doy_id),]
 
 ##### 5 - PLOT TRENDS BY TYPE ####
 
 # yearly trends per site 
-(greenup50 <- ggplot(thresh50) +
+(greenup25 <- ggplot(thresh25) +
     geom_point(aes(x = year, y = phase_doy, colour = phase), alpha = 0.5, size = 2) +
     geom_smooth(method=lm, aes(x = year,  y = phase_doy, fill = phase, colour = phase))) + 
-    ylab("Phenophase DOY (50% of Maximum Curviture) \n") +
+    ylab("Phenophase DOY (25% of Maximum Curviture) \n") +
     xlab("Year") +
   # xlim(2004,2021) +
     scale_color_manual(values = c("#09E88F", "#E0C707")) +
    scale_fill_manual(values = c("#09E88F", "#E0C707")) +
   theme_classic() +
   facet_wrap(vars(type))
-ggsave('figures/50_sen_ts.jpg', width = 9, height = 5, units = 'in', dpi = 400)
+ggsave('figures/25_sen_ts.jpg', width = 9, height = 5, units = 'in', dpi = 400)
 
 
 ##### 6 - MODEL PHENOLOGY CHANGE ####
